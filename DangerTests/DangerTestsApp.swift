0555e7162ce0e08af9b2dfc3576be75891da8907
Philipp Schmid <philipp.schmid@grandcentrix.net>
2022-08-04 10:32:08 +0200

    Initial Commit

---
+++ DangerTests/DangerTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DangerTestsApp.swift
+//  DangerTests
+//
+//  Created by Philipp Schmid on 04.08.22.
+//
+
+import SwiftUI
+
+@main
+struct DangerTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
